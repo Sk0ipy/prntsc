a51103531c164b463bd31de3bd15d3befa39b741
Sk0ipy <74152292+Sk0ipy@users.noreply.github.com>
2024-02-27 13:52:54 +0100

    finishing touches

--- gen_codes.py
+++ gen_codes.py
@@ -1,3 +1,6 @@
+import itertools
+from multiprocessing.pool import ThreadPool
+import time
 import psycopg2
 from psycopg2 import pool
 
@@ -12,16 +15,18 @@ connection_pool = psycopg2.pool.SimpleConnectionPool(
 
 
 def gen_code():
+    print("Generating codes...")  # Toegevoegd
     characters = [chr(i) for i in range(ord('a'), ord('z') + 1)] + [str(i) for i in range(10)]
-    for a in characters:
-        for b in characters:
-            for c in characters:
-                for d in characters:
-                    for e in characters:
-                        yield a + b + c + d + e
+    counter = 0  # Toegevoegd
+    for code in itertools.product(characters, repeat=5):  # Veranderd naar 6
+        counter += 1  # Verhoog de teller
+        if counter % 1000000 == 0:  # Print elke miljoen codes
+            print(f"Generated {counter} codes so far...")
+        yield ''.join(code)
 
 
 def chunk_generator(iterable, chunk_size):
+    print("Generating chunks...")  # Toegevoegd
     chunk = []
     for item in iterable:
         chunk.append(item)
@@ -32,17 +37,72 @@ def chunk_generator(iterable, chunk_size):
         yield chunk
 
 
-def main():
-    gen = gen_code()
-    chunk_size = 30000  # Adjust the chunk size as per your needs
-    with connection_pool.getconn() as connection:
-        with connection.cursor() as cursor:
-            for chunk in chunk_generator(gen, chunk_size):
-                codes = [(code,) for code in chunk]
-                cursor.executemany("INSERT INTO code5 (code) VALUES (%s)", codes)
-                connection.commit()
-        connection_pool.putconn(connection)
+def worker(chunk):
+    print(f"Starting worker function with {len(chunk)} codes...")
+    # Create a new connection for each worker
+    connection = psycopg2.connect(
+        host="localhost",
+        user="postgres",
+        password="1234",
+        database="codes"
+    )
 
+    with connection.cursor() as cursor:
+        # Create a list of tuples for the batch operation
+        codes = [(code,) for code in chunk]
+        print(f"Inserting {len(codes)} codes into the database...")  # Toegevoegd
+
+        # Use executemany to perform the batch operation
+        cursor.executemany("INSERT INTO code5 (code) VALUES (%s)", codes)  # Veranderd naar code6
+
+        # Commit the transaction
+        connection.commit()
+
+    # Close the connection when done
+    connection.close()
+
+
+def main():
+    print("Starting main function...")
+    start_time = time.time()  # Starttijd opslaan
+
+    gen = gen_code()
+    chunk_size = 30000  # Verkleind naar 1000
+
+    # Maak een ThreadPool
+    pool = ThreadPool()
+
+    # Gebruik de map-functie van de pool om het werk toe te wijzen aan de worker-functie
+    print("Starting ThreadPool map...")  # Toegevoegd
+    pool.map(worker, chunk_generator(gen, chunk_size))
+    print("Finished ThreadPool map...")  # Toegevoegd
+
+    # Sluit de pool en wacht tot alle workers klaar zijn
+    pool.close()
+    pool.join()
+
+    end_time = time.time()  # Eindtijd opslaan
+
+    elapsed_time = end_time - start_time  # Bereken de verstreken tijd
+    print(f"Het programma duurde {elapsed_time // 3600} uur en {(elapsed_time % 3600) // 60} minuten.")
+
+def test_database_connection():
+    try:
+        connection = psycopg2.connect(
+            host="localhost",
+            user="postgres",
+            password="1234",
+            database="codes"
+        )
+        connection.close()
+        print("Database is bereikbaar.")
+    except Exception as e:
+        print("Kan geen verbinding maken met de database. Controleer de databasegegevens en de netwerkverbinding.")
+        print("Foutdetails:", e)
+
+
+
+test_database_connection()
 
 if __name__ == '__main__':
     main()
--- get_links.py
+++ get_links.py
@@ -1,54 +1,86 @@
-import mysql.connector
-import mysql.connector.pooling
-import requests
+import asyncio
+import sys
+import time
 
-# create a connection to the database
-connection_pool = mysql.connector.pooling.MySQLConnectionPool(
-    host="localhost",
-    user="root",
-    database="prntsc_db"
-)
+import aiohttp
+import asyncpg
+import select
 
-base_url = "https://i.imgur.com/"  # base url for the images
+# Database connection parameters
+connection_params = {
+    'host': 'localhost',
+    'user': 'postgres',
+    'password': '1234',
+    'database': 'codes',
+}
 
-
-def check_url(url):
-    response = requests.get(url)
-    print(response.url)
-    if response.url == "https://i.imgur.com/removed.png":
-        return False
-    else:
-        return True
-
-
-def main():
-    with connection_pool.get_connection() as connection, connection.cursor() as cursor:
-        while True:
-            try:
-                cursor.execute("SELECT code from codes LIMIT 1")
-                code = cursor.fetchone()
-                if not code:
-                    break
-                code = code[0]
-                url = base_url + code + ".jpg"
-                print(url)
-                if check_url(url):
-                    cursor.execute("INSERT INTO working_urls (url) VALUES (%s)", (url,))
-                    # remove the code from the database
-                    cursor.execute("DELETE FROM codes WHERE code = %s", (code,))
-                    connection.commit()
-                    print(url)
-                else:
-                    # remove the code from the database
-                    cursor.execute("DELETE FROM codes WHERE code = %s", (code,))
-                    connection.commit()
-
-            except Exception as e:
-                print(e)
+# Base URL for the images
+base_url = "https://i.imgur.com/"
+# Table with the codes
+code_table = "code5"
+# Number of concurrent HTTP requests
+concurrent_requests = 10
+# Batch size for processing codes
+batch_size = 1000
 
 
+async def fetch_and_process_codes(pool, session, codes, stats):
+    async with pool.acquire() as connection:
+        async with connection.transaction():
+            for code in codes:
+                if stats['checked'] >= stats['total']:
+                    return
+                await process_code(session, connection, code['code'], stats)
 
+
+async def process_code(session, connection, code, stats):
+    url = base_url + code + ".png"
+    async with session.head(url, allow_redirects=True) as response:
+        final_url = str(response.url)
+        stats['checked'] += 1
+        if final_url == "https://i.imgur.com/removed.png":
+            connection.execute(f"DELETE FROM {code_table} WHERE code = $1", code)
+            print(f"Code {code} URL is not valid.")
+        else:
+            await insert_url(connection, url)
+            stats['valid'] += 1
+            connection.execute(f"DELETE FROM {code_table} WHERE code = $1", code)
+            print(f"Code {code} URL inserted.")
+
+
+async def insert_url(connection, url):
+    async with connection.transaction():
+        await connection.execute("INSERT INTO working_urls (url) VALUES ($1)", url)
+
+
+async def main():
+    stats = {'total': 0, 'checked': 0, 'valid': 0}
+    start_time = time.time()
+    async with asyncpg.create_pool(**connection_params, min_size=1, max_size=20) as pool:
+        async with aiohttp.ClientSession() as session:
+            async with pool.acquire() as connection:
+                async with connection.transaction():
+                    codes = await connection.fetch(f"SELECT code FROM {code_table}")
+                    stats['total'] = len(codes)
+                    print(f"Total codes to process: {stats['total']}")
+
+                    # Process codes in batches
+                    for i in range(0, stats['total'], batch_size):
+                        batch = codes[i:i + batch_size]
+                        await fetch_and_process_codes(pool, session, batch, stats)
+                        if stats['checked'] >= stats['total']:
+                            break
+                        if sys.stdin in select.select([sys.stdin], [], [], 0)[0]:
+                            if sys.stdin.readline().strip() == 'q':
+                                break
+
+    end_time = time.time()
+    duration = end_time - start_time
+    print(f"Time taken: {duration} seconds")
+    print(f"Total codes checked: {stats['checked']}")
+    print(f"Total valid codes: {stats['valid']}")
+    print(f"Percentage of valid codes: {stats['valid'] / stats['checked'] * 100:.2f}%")
 
 
 if __name__ == '__main__':
-    main()
+    asyncio.run(main())
